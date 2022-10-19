3f95dd3e837f0d6fb7815e1aa1e053af448b635a
Maxim Zakharov <maxeozakh@gmail.com>
2022-10-19 09:14:55 +0400

    proper watch for esbuild

--- watch.js
+++ watch.js
@@ -1,8 +1,34 @@
+/* eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef */
 require('esbuild')
-  .build({
-    watch: {},
-    bundle: true,
-    entryPoints: ['index.tsx', 'index.css'],
-    outdir: 'public',
+  .serve(
+    {
+      host: 'localhost',
+      port: 8000,
+      servedir: 'local',
+    },
+    {
+      bundle: true,
+      entryPoints: ['index.tsx', 'index.css'],
+      loader: {
+        '.png': 'file',
+        '.jpg': 'file',
+        '.jpeg': 'file',
+        '.gif': 'file',
+        '.svg': 'file',
+        '.ttf': 'file',
+        '.woff': 'file',
+        '.woff2': 'file',
+        '.eot': 'file',
+        '.otf': 'file',
+      },
+    },
+  )
+  .then((result) => {
+    const { host, port } = result
+    console.log(`serving at http://${host}:${port}`)
   })
-  .catch(() => process.exit(1))
+  .catch((error) => {
+    console.log('watch build failed, error: ', error)
+    /* eslint-disable-next-line no-undef */
+    process.exit(1)
+  })
